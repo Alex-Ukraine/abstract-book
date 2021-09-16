8e26e1d03da7f86d5f1ee2dca4979b59912e388e
oleksii_esaulov <oleksii_esaulov@epam.com>
2021-09-17 01:12:00 +0300

    readability and it's properly parsed location

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from copy import copy
 from typing import List
 
 from openpyxl import load_workbook
-from openpyxl.styles import cell_style
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal, LTChar
 
-file_pdf = 'Abstract Book from the 5th World Psoriasis and Psoriatic Arthritis Conference 2018.pdf'
-file_xml = 'Data Entry - 5th World Psoriasis & Psoriatic Arthritis Conference 2018 - Case format (2).xlsx'
+file_pdf = "Abstract Book from the 5th World Psoriasis and Psoriatic Arthritis Conference 2018.pdf"
+file_xml = "Data Entry - 5th World Psoriasis & Psoriatic Arthritis Conference 2018 - Case format (2).xlsx"
 START_PAGE = 100
-END_PAGE = 104
-SHEET_NAME = 'Sheet1'
+END_PAGE = 155
+SHEET_NAME = "Sheet1"
 START_ROW = 7
 
 
@@ -53,12 +52,12 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
     flag = False
 
     # initial values
-    session_name = ''
-    topic_title = ''
-    name = ''
-    affiliation_name = ''
-    persons_location = ''
-    abstract = ''
+    session_name = ""
+    topic_title = ""
+    name = ""
+    affiliation_name = ""
+    persons_location = ""
+    abstract = ""
     records = []
 
     # walking through pages
@@ -71,42 +70,79 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
 
                 for text_line in element:
                     # let's do some investigation about most frequent font and size of one pdf-line
-                    font_and_size_list = [(character.fontname, character.size) for character in text_line if
-                                          isinstance(character, LTChar)]
+                    font_and_size_list = [
+                        (character.fontname, character.size)
+                        for character in text_line
+                        if isinstance(character, LTChar)
+                    ]
                     font_and_size = Counter(font_and_size_list).most_common(1)[0][0]
 
-                    if font_and_size == ('OAOVWE+TimesNewRomanPS-BoldItalicMT', 9.5):
+                    """
+                    After my research I got interesting correlation:
+                    ("OAOVWE+TimesNewRomanPS-BoldItalicMT", 9.5) correlate with session_name
+                    ("OAOVWE+TimesNewRomanPS-BoldMT", 9.0) correlate with topic_title
+                    ("OAOVWE+TimesNewRomanPS-ItalicMT", 9.0) correlate with name
+                    ("OAOVWE+TimesNewRomanPS-ItalicMT", 8.0) correlate with affiliation_name
+                    """
+
+                    if font_and_size == ("OAOVWE+TimesNewRomanPS-BoldItalicMT", 9.5):
                         base_name = text_line.get_text()
                         if affiliation_name:
                             # we assume that every institutions text-box end's with location,
                             # ... but for most cases it's wrong idea
-                            persons_location = affiliation_name.split(', ')[-1]
-                            if 'University' not in persons_location:
-                                affiliation_name = affiliation_name.replace(', ' + persons_location, '')
+                            persons_location = ", ".join(
+                                affiliation_name.split(", ")[-2:]
+                            )
+                            if "University" not in persons_location:
+                                affiliation_name = affiliation_name.replace(
+                                    ", " + persons_location, ""
+                                )
                             else:
-                                persons_location = ''
+                                persons_location = ""
 
                         current_page = int(base_name[1:])
                         """if current_page < start_page:
                             break"""
 
                         if start_page < current_page <= end_page:
-                            names = name.split(', ')
+                            names = name.split(", ")
                             if names:
                                 for _ in names:
-                                    record = (_.replace('\n', ''), affiliation_name.replace('\n', ''),
-                                              persons_location.replace('\n', ''), session_name.replace('\n', ''),
-                                              topic_title.replace('\n', ''), abstract.replace('-\n', '').replace('\n', ''))
+                                    record = (
+                                        _.replace("-\n", "").replace("\n", ""),
+                                        affiliation_name.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        persons_location.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        session_name.replace("\n", ""),
+                                        topic_title.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        abstract.replace("-\n", "").replace("\n", ""),
+                                    )
                                     records.append(record)
                         # when we encounter with page more than end_page,
                         # ... we need to append accumulated temporary data to results
                         elif current_page > end_page:
-                            names = name.split(', ')
+                            names = name.split(", ")
                             if names:
                                 for _ in names:
-                                    record = (_.replace('\n', ''), affiliation_name.replace('\n', ''),
-                                              persons_location.replace('\n', ''), session_name.replace('\n', ''),
-                                              topic_title.replace('\n', ''), abstract.replace('-\n', '').replace('\n', ''))
+                                    record = (
+                                        _.replace("-\n", "").replace("\n", ""),
+                                        affiliation_name.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        persons_location.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        session_name.replace("\n", ""),
+                                        topic_title.replace("-\n", "").replace(
+                                            "\n", ""
+                                        ),
+                                        abstract.replace("-\n", "").replace("\n", ""),
+                                    )
                                     records.append(record)
                             flag = True
                             break
@@ -115,29 +151,43 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
                         session_name = text_line.get_text()
 
                         # we need to empty temporary accumulators
-                        topic_title = ''
-                        name = ''
-                        affiliation_name = ''
-                        persons_location = ''
-                        abstract = ''
-                    elif font_and_size == ('OAOVWE+TimesNewRomanPS-BoldMT', 9.0):
+                        topic_title = ""
+                        name = ""
+                        affiliation_name = ""
+                        persons_location = ""
+                        abstract = ""
+                    elif font_and_size == ("OAOVWE+TimesNewRomanPS-BoldMT", 9.0):
                         # lets filter line from super-scripted numbers of references
-                        line = ''.join(x.get_text() for x in text_line if isinstance(x, LTChar) and x.size == 9.0)
+                        line = "".join(
+                            _.get_text()
+                            for _ in text_line
+                            if isinstance(_, LTChar) and _.size == 9.0
+                        )
                         topic_title += line
                         # topic_title += text_line.get_text()
-                    elif font_and_size == ('OAOVWE+TimesNewRomanPS-ItalicMT', 9.0):
-                        line = ''.join(x.get_text() for x in text_line if isinstance(x, LTChar) and x.size == 9.0)
+                    elif font_and_size == ("OAOVWE+TimesNewRomanPS-ItalicMT", 9.0):
+                        line = "".join(
+                            _.get_text()
+                            for _ in text_line
+                            if isinstance(_, LTChar) and _.size == 9.0
+                        )
                         # check for absence 'References:' or 'Reference:' text-line and only then add line
-                        if ':' not in line:
+                        if ":" not in line:
                             name += line
-                    elif font_and_size == ('OAOVWE+TimesNewRomanPS-ItalicMT', 8.0):
-                        line = ''.join(x.get_text() for x in text_line if isinstance(x, LTChar) and x.size == 8.0)
-                        if ':' not in line:
+                    elif font_and_size == ("OAOVWE+TimesNewRomanPS-ItalicMT", 8.0):
+                        line = "".join(
+                            _.get_text()
+                            for _ in text_line
+                            if isinstance(_, LTChar) and _.size == 8.0
+                        )
+                        if ":" not in line:
                             affiliation_name += line
                     else:
                         abstract += text_line.get_text()
     return records
 
 
-if __name__ == '__main__':
-    update_xml(file_xml, parse_articles(file_pdf, START_PAGE, END_PAGE), SHEET_NAME, START_ROW)
+if __name__ == "__main__":
+    update_xml(
+        file_xml, parse_articles(file_pdf, START_PAGE, END_PAGE), SHEET_NAME, START_ROW
+    )
