1c04d0f79a100c581039a95d53b3520fb6d779dc
oleksii_esaulov <oleksii_esaulov@epam.com>
2021-09-16 22:29:49 +0300

    added more readability

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from collections import Counter
+from copy import copy
 from typing import List
 
 from openpyxl import load_workbook
+from openpyxl.styles import cell_style
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal, LTChar
 
@@ -10,11 +12,12 @@ file_xml = 'Data Entry - 5th World Psoriasis & Psoriatic Arthritis Conference 20
 START_PAGE = 100
 END_PAGE = 104
 SHEET_NAME = 'Sheet1'
-START_ROW = 6
+START_ROW = 7
 
 
 def update_xml(file: str, rec: list, sheet_name: str, start_row: int) -> None:
     """
+    function load's xml file and then put's list of rows 'rec', start's with 'start_row'
     :param file: file xml for read and update with bunch of records
     :param rec: list of tuples aka records, we expect that every tuple consists 6 values
     :param sheet_name: name of sheet that has to be updated
@@ -24,7 +27,15 @@ def update_xml(file: str, rec: list, sheet_name: str, start_row: int) -> None:
     ws = wb[sheet_name]
     for row in range(start_row, len(rec) + start_row):
         for col in range(1, 7):
-            ws.cell(row=row, column=col).value = rec[row - 6][col - 1]
+            ws.cell(row=row, column=col).value = rec[row - start_row][col - 1]
+            cell = ws["A3"]
+            if cell.has_style:
+                ws.cell(row=row, column=col).font = copy(cell.font)
+                ws.cell(row=row, column=col).border = copy(cell.border)
+                ws.cell(row=row, column=col).fill = copy(cell.fill)
+                ws.cell(row=row, column=col).number_format = copy(cell.number_format)
+                ws.cell(row=row, column=col).protection = copy(cell.protection)
+                ws.cell(row=row, column=col).alignment = copy(cell.alignment)
 
     wb.save(file)
     wb.close()
@@ -32,6 +43,7 @@ def update_xml(file: str, rec: list, sheet_name: str, start_row: int) -> None:
 
 def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
     """
+    function parses pdf-file and return's list of tuples with received data
     :param file: pdf-file which we want to parse
     :param start_page: number of page where we start to parse
     :param end_page: number of page where we end to parse
@@ -49,10 +61,11 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
     abstract = ''
     records = []
 
+    # walking through pages
     for page_layout in extract_pages(file):
         if flag:
             break
-
+        # walking through pdf basic elements
         for element in page_layout:
             if isinstance(element, LTTextBoxHorizontal):
 
@@ -65,8 +78,13 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
                     if font_and_size == ('OAOVWE+TimesNewRomanPS-BoldItalicMT', 9.5):
                         base_name = text_line.get_text()
                         if affiliation_name:
-                            persons_location = affiliation_name.split(',')[-1]
-                            affiliation_name = affiliation_name.replace(', ' + persons_location, '')
+                            # we assume that every institutions text-box end's with location,
+                            # ... but for most cases it's wrong idea
+                            persons_location = affiliation_name.split(', ')[-1]
+                            if 'University' not in persons_location:
+                                affiliation_name = affiliation_name.replace(', ' + persons_location, '')
+                            else:
+                                persons_location = ''
 
                         current_page = int(base_name[1:])
                         """if current_page < start_page:
@@ -80,6 +98,8 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
                                               persons_location.replace('\n', ''), session_name.replace('\n', ''),
                                               topic_title.replace('\n', ''), abstract.replace('-\n', '').replace('\n', ''))
                                     records.append(record)
+                        # when we encounter with page more than end_page,
+                        # ... we need to append accumulated temporary data to results
                         elif current_page > end_page:
                             names = name.split(', ')
                             if names:
@@ -91,8 +111,10 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
                             flag = True
                             break
 
+                        # we need to remember last saved number of article
                         session_name = text_line.get_text()
 
+                        # we need to empty temporary accumulators
                         topic_title = ''
                         name = ''
                         affiliation_name = ''
@@ -105,6 +127,7 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
                         # topic_title += text_line.get_text()
                     elif font_and_size == ('OAOVWE+TimesNewRomanPS-ItalicMT', 9.0):
                         line = ''.join(x.get_text() for x in text_line if isinstance(x, LTChar) and x.size == 9.0)
+                        # check for absence 'References:' or 'Reference:' text-line and only then add line
                         if ':' not in line:
                             name += line
                     elif font_and_size == ('OAOVWE+TimesNewRomanPS-ItalicMT', 8.0):
@@ -116,4 +139,5 @@ def parse_articles(file: str, start_page: int, end_page: int) -> List[tuple]:
     return records
 
 
-update_xml(file_xml, parse_articles(file_pdf, START_PAGE, END_PAGE), SHEET_NAME, START_ROW)
+if __name__ == '__main__':
+    update_xml(file_xml, parse_articles(file_pdf, START_PAGE, END_PAGE), SHEET_NAME, START_ROW)
